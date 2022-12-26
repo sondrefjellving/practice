43af26fa632eefca4ecf1d0207d1c1933a21c006
Sondre Fjellving Andersen <54597015+sondrefjellving@users.noreply.github.com>
2022-12-26 23:57:29 +0100

    Add unfinished practice task

---
+++ Hangman.java
@@ -0,0 +1,127 @@
+import java.util.Scanner;
+
+public class Hangman {
+    public static Scanner scan = new Scanner(System.in);
+
+    public static String[] words = {"ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
+    "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer",
+    "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat",
+    "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose",
+    "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", 
+    "python", "rabbit", "ram", "rat", "raven","rhino", "salmon", "seal",
+    "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan",
+    "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf",
+    "wombat", "zebra"};
+
+    public static String[] gallows = {"+---+\n" +
+    "|   |\n" +
+    "    |\n" +
+    "    |\n" +
+    "    |\n" +
+    "    |\n" +
+    "=========\n",
+
+    "+---+\n" +
+    "|   |\n" +
+    "O   |\n" +
+    "    |\n" +
+    "    |\n" +
+    "    |\n" +
+    "=========\n",
+
+    "+---+\n" +
+    "|   |\n" +
+    "O   |\n" +
+    "|   |\n" +
+    "    |\n" +
+    "    |\n" +
+    "=========\n",
+
+    " +---+\n" +
+    " |   |\n" +
+    " O   |\n" +
+    "/|   |\n" +
+    "     |\n" +
+    "     |\n" +
+    " =========\n",
+
+    " +---+\n" +
+    " |   |\n" +
+    " O   |\n" +
+    "/|\\  |\n" +
+    "     |\n" +
+    "     |\n" +
+    " =========\n",
+
+    " +---+\n" +
+    " |   |\n" +
+    " O   |\n" +
+    "/|\\  |\n" +
+    "/    |\n" +
+    "     |\n" +
+    " =========\n",
+
+    " +---+\n" +
+    " |   |\n" +
+    " O   |\n" +
+    "/|\\  |\n" + 
+    "/ \\  |\n" +
+    "     |\n" +
+    " =========\n"};
+
+    public static void main(String[] args) {
+        startGame();
+    }
+
+    public static void startGame() {
+        int wrongGuesses = 0;
+        String guess = "";
+
+        while (wrongGuesses < 6) {
+            printLastGuess(guess);
+            printGallow(wrongGuesses);
+            guess = scan.nextLine();
+            wrongGuesses++;
+        }
+        System.out.println("END GAME");
+        //print guess
+    }
+
+    /**
+     * Gets a number in the range (0 - length of words array) and selects a word
+     * by using indexing.
+     *
+     * @return the random word - (String)
+     */
+    public static String randomWord() {
+        int randomIndex = (int) (Math.random() * words.length);
+        return words[randomIndex];
+    }
+
+
+    /**
+     * Prints the gallow.
+     *
+     * @param wrongGuesses the number of wrong guesses - (int)
+     */
+    public static void printGallow(int wrongGuesses) {
+        System.out.print(gallows[wrongGuesses]);
+    }
+
+
+    public static void printLastGuess(String guess) {
+        System.out.print("Guess:\t" + guess);
+    }
+
+
+
+
+
+
+
+}
+
+
+
+
+
