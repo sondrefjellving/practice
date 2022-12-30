dad699b09185ce19db922d0d7f09c74b9e4fe891
Sondre Fjellving Andersen <54597015+sondrefjellving@users.noreply.github.com>
2022-12-30 09:57:23 +0100

    Add final version of Hangman.java
    Fixed unintended bug that only updated the first instance of a character in the placeholder 
    (eg: word is 'moose'. User types 'o', but placeholder only updates to '_ o _ _ _' when it should be '_ o o _ _'), and updated JavaDocs.
    
    This task is from The Complete Development Bootcamp course on Udemy (link in README).

--- Hangman.java
+++ Hangman.java
@@ -1,9 +1,20 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * A game of Hangman. The word gets chosen randomly from the words list. The user can
+ * guess wrong six times before they lose.
+ * This task is from The Complete Development Bootcamp course on Udemy (link in README).
+ * 
+ * @author sondrefjellvingandersen 
+ */
 public class Hangman {
     public static Scanner scan = new Scanner(System.in);
 
+    /**
+     * The random words to be used in the game.
+     */
     public static String[] words = {"ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
     "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer",
     "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat",
@@ -14,6 +25,9 @@ public class Hangman {
     "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf",
     "wombat", "zebra"};
 
+    /**
+     * The gallows that gets updated every time the user guesses wrong.
+     */
     public static String[] gallows = {"+---+\n" +
     "|   |\n" +
     "    |\n" +
@@ -74,6 +88,10 @@ public class Hangman {
         startGame();
     }
 
+
+    /**
+     * Starts the game.
+     */
     public static void startGame() {
         String[] word = randomWord();
         String[] placeholder = createPlaceholder(word);
@@ -99,7 +117,7 @@ public class Hangman {
 
             if (checkGuess(guess, word)) { // if correct
                 placeholder = updatePlaceholder(placeholder, word, guess);
-            } else if(misses.contains(guess)) { // if guess is wrong and already guessed
+            } else if (misses.contains(guess)) { // if guess is wrong and already guessed
                 System.out.print("You have already guessed this character\n");
             } else { // if guess is wrong
                 misses = updateMisses(misses, guess);
@@ -145,7 +163,7 @@ public class Hangman {
         return guess;
     }
 
-    
+
     /**
      * Checks if the guess that is sent in as argument exists in the word that it will be
      * checked against.
@@ -162,6 +180,7 @@ public class Hangman {
         }
         return false;
     }
+
 
     /**
      * Prints the last guess.
@@ -187,35 +206,80 @@ public class Hangman {
     }
 
 
+    /**
+     * Updates the placeholder by adding the correct guess to the 'hidden' word.
+     *
+     * @param placeholder the placeholder that gets updated - (String[])
+     * @param word the word to be checked against           - (String[])
+     * @param guess the users guess                         - (String)
+     * @return the updated placeholder                      - (String[])
+     */
     public static String[] updatePlaceholder(String[] placeholder, String[] word, String guess) {
-        int characterIndex = findCharIndex(word, guess);
-        placeholder[characterIndex] = guess;
+        ArrayList<Integer> characterIndex = findCharIndex(word, guess);
+        for (int i = 0; i < characterIndex.size(); i++) {
+            placeholder[characterIndex.get(i)] = guess;
+        }
         return placeholder;
     }
 
-    // find all matches???
-    public static int findCharIndex(String[] word, String guess) {
+
+    /**
+     * Finds all matches of current guess and stores the indexes in an array that gets returned.
+     *
+     * @param word the word that contains the solution  - (String[])
+     * @param guess the users guess                     - (String)
+     * @return the character indexes of the right guess - (ArrayList<Integer>)
+     */
+    public static ArrayList<Integer> findCharIndex(String[] word, String guess) {
+        ArrayList<Integer> charIndexes = new ArrayList<Integer>();
         for (int i = 0; i < word.length; i++) {
             if (word[i].equalsIgnoreCase(guess)) {
-                return i;
+                charIndexes.add(i);
             }
         }
-        return 0;
+        return charIndexes;
     }
 
+
+    /**
+     * Prints the placeholder.
+     *
+     * @param placeholder the placeholder that gets printed - (String[])
+     */
     public static void printPlaceholder(String[] placeholder) {
         System.out.print("\nWord:\t" + String.join(" ", placeholder));
     }
 
 
+    /**
+     * Prints the characters that were misses.
+     *
+     * @param misses the missed characters - (String)
+     */
     public static void printMisses(String misses) {
         System.out.print("\n\nMisses:\t" + misses);
     }
 
+
+    /**
+     * Updates the missed characters by adding them to the previous 'misses' String.
+     *
+     * @param misses the previously missed characters - (String)
+     * @param guess the users guess                   - (String)
+     * @return the updated missed characters String   - (String)
+     */
     public static String updateMisses(String misses, String guess) {
         return misses + guess;
     }
 
+
+    /**
+     * Checks if the user wins by comparing all the characters.
+     *
+     * @param placeholder the placeholder that contains the users guesses - (String[])
+     * @param word the word that the user tries to guess                  - (String[])
+     * @return the result                                                 - (boolean)
+     */
     public static boolean checkWin(String[] placeholder, String[] word) {
         for (int i = 0; i < placeholder.length; i++) {
             if (!placeholder[i].equalsIgnoreCase(word[i])) {
@@ -226,6 +290,12 @@ public class Hangman {
     }
 
 
+    /**
+     * Converts a String array to normal String.
+     *
+     * @param word the String array that gets converted - (String[])
+     * @return the word in String format                - (String)
+     */
     public static String wordToString(String[] word) {
         return String.join("", word);
     }
